370e2a291b0fa28b338ec996dcc570e95a478e7d
shivin_vijai <shivin.vijai@oracle.com>
2019-04-18 16:39:55 +0800

    Added Terraform Installation Sctipt

---
+++ installTerraform.ps1
@@ -0,0 +1,19 @@
+$tflatestversion = "0.11.13"
+$status = terraform -v
+
+#Write-Host $output
+
+if ($? -eq "True") {
+		write-host "Terraform Already Installed, version is :" $status
+	}
+Else {
+		write-host "Going to install Terraform"
+		$url = "https://releases.hashicorp.com/terraform/$tflatestversion/terraform_$($tflatestversion)_windows_amd64.zip"
+		$output = "$PSScriptRoot\terraform_$($tflatestversion)_windows_amd64.zip"
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		Invoke-WebRequest -Uri $url -OutFile $output
+		expand-archive -path terraform_$($tflatestversion)_windows_amd64.zip -destinationpath 'terraform'
+		$env:Path += ";$PSScriptRoot\terraform"
+		$status = terraform -v
+		write-host "Terraform Installed, version is :" $status
+	}
